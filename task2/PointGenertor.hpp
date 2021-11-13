d7912a442940327d4c67ae720f75b887b2a646c6
LittleLovelySun <sd.kudinova@gmail.com>
2021-11-13 14:13:40 +0300

    add parallel point generation

--- task2/PointGenertor.hpp
+++ task2/PointGenertor.hpp
@@ -3,13 +3,17 @@
 #include <iostream>
 #include <vector>
 #include <cstring>
+#include <cmath>
+#include <cfloat>
+
 #include "Entities.hpp"
 
 
 enum FillMode {
     Ascending,
     Descending,
-    Random
+    Random,
+    Trigonometry
 };
 
 FillMode GetFillMode(const char* mode) {
@@ -19,25 +23,37 @@ FillMode GetFillMode(const char* mode) {
         return Descending;
     if (!strcmp("random", mode) || !strcmp("r", mode))
         return Random;
+    if (!strcmp("trigonometry", mode) || !strcmp("t", mode))
+        return Trigonometry;
 
-    std::cout << "Warning: invalid fill mode (" << mode << "). Use random by default" << std::endl;
-    return Random;
+    std::cout << "Warning: invalid fill mode (" << mode << "). Use trigonometry by default" << std::endl;
+    return Trigonometry;
 }
 
 Point GetPoint(int i, int j, int n1, int n2, FillMode mode) {
+    int total = n1 * n2;
     int index = i * n2 + j;
     float x, y;
+
+    if (index >= total) {
+        return (Point) {{FLT_MAX, FLT_MAX}, -1};
+    }
+
     if (mode == Ascending) {
         x = index;
         y = index;
     }
     else if (mode == Descending) {
-        x = n1 * n2 - index - 1;
+        x = total - index - 1;
         y = x;
     }
+    else if (mode == Random) {
+        x = rand() % total;
+        y = rand() % total;
+    }
     else {
-        x = rand() % (n1 * n2);
-        y = rand() % (n1 * n2);
+        x = sin(index * 2 * M_PI / total);
+        y = cos(index * 2 * M_PI / total);
     }
 
     return (Point) {{x, y}, index};
--- task2/task2.cpp
+++ task2/task2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <mpi.h>
 
 #include "Entities.hpp"
 #include "PointGenertor.hpp"
@@ -7,25 +8,48 @@
 using namespace std;
 
 
+vector<Point> GetPoints(int n1, int n2, int rank, int count, FillMode mode) {
+    vector<Point> array(count);
+
+    for (int index = 0; index < count; index++) {
+        int i = (index + rank * count) / n2;
+        int j = (index + rank * count) % n2;
+
+        array[index] = GetPoint(i, j, n1, n2, mode);
+    }
+
+    return array;
+}
+
+
 int main(int argc, const char** argv) {
+    int size, rank;
+    MPI_Init(NULL, NULL);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    srand(rank);
+
     int n1 = atoi(argv[1]);
     int n2 = atoi(argv[2]);
     int sortedCoord = argc <= 3 ? 0 : atoi(argv[3]);
-    FillMode mode = argc <= 4 ? Random : GetFillMode(argv[4]);
+    FillMode mode = argc <= 4 ? Trigonometry : GetFillMode(argv[4]);
 
     int n = n1 * n2;
+    int count = (n + size - 1) / size;
 
-    vector<Point> array(n);
-    cout << "Input array  : ";
+    vector<Point> array = GetPoints(n1, n2, rank, count, mode);
+    cout << "Input array " << rank << ": ";
 
-    for (int i = 0; i < n; i++) {
-        array[i] = GetPoint(i / n2, i % n2, n1, n2, mode);
+    for (int i = 0; i < count; i++) {
         cout << array[i] << " ";
     }
 
-    cout << endl << "Sorted array: ";
-    for (int i = 0; i < n; i++)
-        cout << array[i] << " ";
+    cout << endl;
 
+    // cout << endl << "Sorted array: ";
+    // for (int i = 0; i < n; i++)
+    //     cout << array[i] << " ";
+    MPI_Finalize();
     return 0;
 }
