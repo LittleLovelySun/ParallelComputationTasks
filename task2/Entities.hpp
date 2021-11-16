f4dbe2507007c65a7c294f87094649cdf0bf7bdf
LittleLovelySun <sd.kudinova@gmail.com>
2021-11-17 01:29:15 +0300

    Add diffrent sorting, debug mode and help

--- task2/Entities.hpp
+++ task2/Entities.hpp
@@ -2,10 +2,32 @@
 #include <iostream>
 
 
+enum SortAlgorithm {
+    MergeSortAlgorithm,
+    HeapSortAlgorithm,
+    StdSortAlgorithm
+};
+
 struct Point {
     float coord[2];
     int index;
 };
+
+
+SortAlgorithm GetAlgorithm(const char *arg) {
+    if (!strcmp(arg, "heapsort"))
+        return HeapSortAlgorithm;
+
+    if (!strcmp(arg, "mergesort"))
+        return MergeSortAlgorithm;
+
+    if (!strcmp(arg, "std"))
+        return StdSortAlgorithm;
+
+    std::cout << "Warning: invalid sorting algorithm \"" << arg << "\". Used std" << std::endl;
+
+    return StdSortAlgorithm;
+}
 
 std::ostream& operator <<(std::ostream& os, const Point& point) {
     return os << point.index << ": {" << point.coord[0] << " " << point.coord[1] << "}";
--- task2/task2.cpp
+++ task2/task2.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <algorithm>
 #include <fstream>
+#include <cassert>
+#include <cstring>
 
 #include <mpi.h>
 
@@ -9,6 +11,7 @@
 #include "PointGenertor.hpp"
 
 using namespace std;
+
 
 class PointComparator {
     int sortedCoord;
@@ -34,15 +37,19 @@ class ParallelBetcherSorter {
 
     PointComparator comparator;
 
+    void Heapify(vector<Point> &points, size_t n, size_t i);
+    void HeapSort(vector<Point> &points);
+    void MergeSort(vector<Point> &points, int left, int right);
+
     void MakePointType();
     void MakeMergePoints(const vector<Point> &pointsI, const vector<Point> &pointsJ, vector<Point> &points);
     void MergePoints(vector<Point> &pointsI, int pi, int pj);
     void Merge(int leftPos, int rightPos, int leftSize, int rightSize, int offset, vector<Point> &points);
-    void Sort(int position, int length, vector<Point> &points);
 
+    void Sort(int position, int length, vector<Point> &points);
 public:
     ParallelBetcherSorter(int rank, int size, int sortedCoord);
-    void Sort(vector<Point> &points);
+    void Sort(vector<Point> &points, SortAlgorithm algorithm);
 };
 
 ParallelBetcherSorter::ParallelBetcherSorter(int rank, int size, int sortedCoord): comparator(sortedCoord) {
@@ -71,6 +78,65 @@ void ParallelBetcherSorter::MakePointType() {
 
     MPI_Type_create_struct(2, lengths, displacements, types, &pointType);
     MPI_Type_commit(&pointType);
+}
+
+void ParallelBetcherSorter::Heapify(vector<Point> &points, size_t n, size_t i) {
+    size_t largest = i;
+
+    while (true) {
+        size_t left = 2 * i + 1;
+        size_t right = 2 * i + 2;
+
+        if (left < n && comparator(points[largest], points[left]))
+            largest = left;
+
+        if (right < n && comparator(points[largest], points[right]))
+            largest = right;
+
+        if (largest == i)
+            return;
+
+        swap(points[i], points[largest]);
+        i = largest;
+    }
+}
+
+void ParallelBetcherSorter::HeapSort(vector<Point> &points) {
+    for (size_t i = points.size() / 2 - 1; i + 1 > 0; i--) {
+        Heapify(points, points.size(), i);
+    }
+
+    for (size_t i = points.size() - 1; i + 1 > 0; i--) {
+        swap(points[0], points[i]);
+        Heapify(points, i, 0);
+    }
+}
+
+void ParallelBetcherSorter::MergeSort(vector<Point> &points, int left, int right) {
+    if (left + 1 > right)
+        return;
+
+    int mid = left + (right - left) / 2;
+    MergeSort(points, left, mid);
+    MergeSort(points, mid + 1, right);
+    
+    vector<Point> tmp(right - left + 1);
+
+    size_t i = left;
+    size_t j = mid + 1;
+    size_t k = 0;
+
+    while (i <= mid && j <= right)
+        tmp[k++] = comparator(points[i], points[j]) ? points[i++] : points[j++];
+
+    while (i <= mid)
+        tmp[k++] = points[i++];
+
+    while (j <= right)
+        tmp[k++] = points[j++];
+
+    for (size_t i = left; i <= right; i++)
+        points[i] = tmp[i - left];
 }
 
 void ParallelBetcherSorter::MakeMergePoints(const vector<Point> &pointsI, const vector<Point> &pointsJ, vector<Point> &points) {
@@ -144,8 +210,17 @@ void ParallelBetcherSorter::Sort(int position, int length, vector<Point> &points
 	Merge(position, position + middle, middle, length - middle, 1, points);
 }
 
-void ParallelBetcherSorter::Sort(vector<Point> &points) {
-    sort(points.begin(), points.end(), comparator);
+void ParallelBetcherSorter::Sort(vector<Point> &points, SortAlgorithm algorithm) {
+    if (algorithm == HeapSortAlgorithm) {
+        HeapSort(points);
+    }
+    else if (algorithm == MergeSortAlgorithm) {
+        MergeSort(points, 0, points.size() - 1);
+    }
+    else {
+        sort(points.begin(), points.end(), comparator);
+    }
+
     Sort(0, size, points);
 }
 
@@ -170,9 +245,35 @@ void PrintPoints(const vector<Point> &points) {
     cout << endl;
 }
 
+void Help() {
+    cout << "Usage: ./task2 n1 n2 [coord] [mode] [path] [algorithm] [debug]" << endl;
+    cout << "Arguments:" << endl;
+    cout << "  n1        - first size of grid" << endl;
+    cout << "  n2        - second size of grid" << endl;
+    cout << "  coord     - coordinate for sorting (0 - x, 1 - y), default is 0 (x)" << endl;
+    cout << "  mode      - mode of filling points, default is trigonometry" << endl;
+    cout << "  path      - path to file for saving results, default 'results.jsonl'" << endl;
+    cout << "  algorithm - sorting algorithm, default std::sort" << endl;
+    cout << "  debug     - debug mode for printing points, not used by default" << endl << endl;
 
+    cout << "Filling modes:" << endl;
+    cout << "  a - ascending (x = y = index)" << endl;
+    cout << "  d - descending (x = y = size - index - 1)" << endl;
+    cout << "  r - random" << endl;
+    cout << "  t - trigonometry (x = sin(phi), y = cos(phi), where phi = 2pi * index / size" << endl << endl;
+
+    cout << "Algorithms:" << endl;
+    cout << "  std       - standard sort (std::sort)" << endl;
+    cout << "  mergesort - merge sort algorithm" << endl;
+    cout << "  heapsort  - heap sort algorithm" << endl;
+}
 
 int main(int argc, const char** argv) {
+    if (argc == 1 || (argc == 2 && (!strcmp(argv[1], "--help") || !strcmp(argv[1], "-h")))) {
+        Help();
+        return 0;
+    }
+
     int size, rank;
     MPI_Init(NULL, NULL);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -185,25 +286,33 @@ int main(int argc, const char** argv) {
     int sortedCoord = argc <= 3 ? 0 : atoi(argv[3]);
     FillMode mode = argc <= 4 ? Trigonometry : GetFillMode(argv[4]);
     const char* path = argc <= 5 ? "results.jsonl" : argv[5];
+    SortAlgorithm algorithm = argc <= 6 ? StdSortAlgorithm : GetAlgorithm(argv[6]);
+    bool debug = argc == 8;
 
     int n = n1 * n2;
     int count = (n + size - 1) / size;
 
     vector<Point> points = GetPoints(n1, n2, rank, count, mode);
-    // cout << "Input points " << rank << ": ";
-    // PrintPoints(points);
+
+    if (debug) {
+        cout << "Input points " << rank << ": ";
+        PrintPoints(points);    
+    }
+    
     MPI_Barrier(MPI_COMM_WORLD);
 
     double start = MPI_Wtime();
     ParallelBetcherSorter sorter(rank, size, sortedCoord);
-    sorter.Sort(points);
+    sorter.Sort(points, algorithm);
     
     double delta = MPI_Wtime() - start;
     double time;
     MPI_Reduce(&delta, &time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
 
-    // cout << "Sorted points " << rank << ": "; 
-    // PrintPoints(points);
+    if (debug) {
+        cout << "Sorted points " << rank << ": "; 
+        PrintPoints(points);
+    }
 
     if (rank == 0) {
         cout << "n1: " << n1 << endl;
